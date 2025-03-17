82fad6de225aaa6fbc1fe95ee691408f533710f3
ILYES <ILYES@OnTheFlux>
2025-03-17 18:43:15 +0100

    creamos la primera clase persona

---
+++ Ejemplo2/src/paquete2/Persona.java
@@ -0,0 +1,30 @@
+package paquete2;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	} 
+	
+	public void mostrarInformacion() {
+		System.out.println("Nombre: " + nombre);
+		System.out.println("edad: " + edad);
+	}
+
+}
+
---
+++ Ejemplo2/src/paquete2/estudiante.java
@@ -0,0 +1,23 @@
+package paquete2;
+
+public class estudiante extends Persona  {
+private  String matricula;
+
+public estudiante(String matricula) {
+	super(matricula );
+	this.matricula = matricula;
+}
+
+public String getMatricula() {
+	return matricula;
+}
+
+public void setMatricula(String matricula) {
+	this.matricula = matricula;
+}
+@Override
+public void mostrarInfromacion () {
+	System.out.println("Matricula "+ matricula );
+	
+}
+}
